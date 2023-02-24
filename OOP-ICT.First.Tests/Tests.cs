a390ddec04b18729f92db076922ee6acc1e7c209
Iskander Kudashev <grinerplaydark@gmail.com>
2023-02-24 15:49:03 +0300

    init repo

---
+++ OOP-ICT.First.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using Xunit.Sdk;
+
+namespace OOP_ICT.Dealer.Tests;
+
+public class Tests : TestClass
+{
+    // TODO: Обратите внимание, что для коллекций и проверок есть разные виды Assert
+    [Fact]
+    public void CheckTestIsWorking_CorrectBuild()
+    {
+        Assert.True(true);
+    }
+}
